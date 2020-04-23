8dfb83a90f5c50128229a8fd17d85afb781c9b8c
Frederico Schardong <frede.sch@gmail.com>
2020-04-23 18:20:19 -0300

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# programming-exercise-2-logistic-regression-university-of-stanford
-Python implementation of "Programming Exercise 2: Logistic Regression" from https://www.coursera.org/learn/machine-learning
+# Programming Exercise 2: Logistic Regression
+This repository provides a Python implementation that solves both the linear and non-linear regression excercises proposed in "Programming Exercise 2: Logistic Regression" from Stanford's Professor [Andrew Ng](http://www.robotics.stanford.edu/~ang/contact.html) at [Coursera](https://www.coursera.org/learn/machine-learning).
+
+A brief analysis of the results is [provided in Portuguese](https://github.com/fredericoschardong/programming-exercise-2-logistic-regression-university-of-stanford/blob/master/Brief%20explanation%20in%20Portuguese.pdf). It was submited as an assignment of a graduate course named [Connectionist Artificial Intelligence](https://moodle.ufsc.br/mod/assign/view.php?id=2122514) at UFSC, Brazil.
--- ex2_reg.py
+++ ex2_reg.py
@@ -6,6 +6,39 @@ np.set_printoptions(suppress=True)
 
 from ex2 import hypothesis, plot_cost_history
 
+def plot_classification(positive, negative, theta, title):
+	#from https://towardsdatascience.com/andrew-ngs-machine-learning-course-in-python-regularized-logistic-regression-lasso-regression-721f311130fb
+	def mapFeaturePlot(x1,x2,degree):
+		"""
+		take in numpy array of x1 and x2, return all polynomial terms up to the given degree
+		"""
+		out = np.ones(1)
+		for i in range(1,degree+1):
+			for j in range(i+1):
+				terms= (x1**(i-j) * x2**j)
+				out= np.hstack((out,terms))
+		return out
+		
+	plt.clf()
+	plt.title(title)
+	plt.plot(positive[:,0], positive[:,1], 'b+', label = 'y = 1')
+	plt.plot(negative[:,0], negative[:,1], 'rx', label = 'y = 0')
+
+	# Plotting decision boundary
+	u_vals = np.linspace(-1,1.5,50)
+	v_vals = np.linspace(-1,1.5,50)
+	z = np.zeros((len(u_vals),len(v_vals)))
+
+	for i in range(len(u_vals)):
+		for j in range(len(v_vals)):
+			z[i,j] = mapFeaturePlot(u_vals[i],v_vals[j],6) @ theta
+			
+	plt.contour(u_vals,v_vals,z.T,0)
+	plt.ylabel('Microchip Test 2')
+	plt.xlabel('Microchip Test 1')
+	plt.legend()
+	plt.show()
+
 # Import data
 data = np.loadtxt("ex2data2.txt", delimiter=",")
 
@@ -20,7 +53,7 @@ plt.plot(positive[:,0], positive[:,1], 'b+', label = 'y = 1')
 plt.plot(negative[:,0], negative[:,1], 'rx', label = 'y = 0')
 
 plt.legend()
-#plt.show()
+plt.show()
 
 # 2.2 Feature mapping
 def map_feature(x1, x2):
@@ -71,64 +104,37 @@ def gradient(theta, X, y, Lambda):
 	return theta
 	
 # There is no fminunc in scipy, so I have used gradient descent with manually tuned alpha and iterations.
-theta = np.zeros(X.shape[1])
-alpha = 0.1
-iterations = 1000
-Lambda = 0
-cost_history = []
+for iterations in [100, 1000]:
+	for Lambda in [0, 0.5]:
+		theta = np.zeros(X.shape[1])
+		alpha = 0.1
+		cost_history = []
 
-m = X.shape[0]
+		m = X.shape[0]
 
-for i in range(iterations):
-	m = X.shape[0]
+		for i in range(iterations):
+			m = X.shape[0]
 
-	h = hypothesis(theta, X)
-	
-	# for j = 0
-	theta[0] = theta[0] - alpha * np.sum((h - y) * X[:, 0]) / m
-	
-	# for j ≥ 1
-	for j in range(1, theta.shape[0]):
-		theta[j] = theta[j] - alpha * (np.sum((h - y) * X[:, j]) / m) + ((Lambda / m) * theta[j])
+			h = hypothesis(theta, X)
+			
+			# for j = 0
+			theta[0] = theta[0] - alpha * np.sum((h - y) * X[:, 0]) / m
+			
+			# for j ≥ 1
+			for j in range(1, theta.shape[0]):
+				theta[j] = theta[j] - alpha * (np.sum((h - y) * X[:, j]) / m) + ((Lambda / m) * theta[j])
+				
+			cost_history.append(cost_function_reg(theta, X, y, Lambda))
+			
+		#plot_cost_history(cost_history)
+		cost = cost_function_reg(theta, X, y, Lambda)
 		
-	cost_history.append(cost_function_reg(theta, X, y, Lambda))
-	
-plot_cost_history(cost_history)
+		# The predict function will produce “1” or “0” predictions given a dataset 
+		# and a learned parameter vector θ.
+		accuracy = np.mean((hypothesis(theta, X) >= 0.5).astype(float) == y) * 100
 
-cost = cost_function_reg(theta, X, y, Lambda)
-
-# The predict function will produce “1” or “0” predictions given a dataset 
-# and a learned parameter vector θ.
-print('Train Accuracy: ', np.mean((hypothesis(theta, X) >= 0.5).astype(float) == y) * 100)
-
-#from https://towardsdatascience.com/andrew-ngs-machine-learning-course-in-python-regularized-logistic-regression-lasso-regression-721f311130fb
-def mapFeaturePlot(x1,x2,degree):
-	"""
-	take in numpy array of x1 and x2, return all polynomial terms up to the given degree
-	"""
-	out = np.ones(1)
-	for i in range(1,degree+1):
-		for j in range(i+1):
-			terms= (x1**(i-j) * x2**j)
-			out= np.hstack((out,terms))
-	return out
-	
-plt.clf()
-plt.plot(positive[:,0], positive[:,1], 'b+', label = 'y = 1')
-plt.plot(negative[:,0], negative[:,1], 'rx', label = 'y = 0')
-
-# Plotting decision boundary
-u_vals = np.linspace(-1,1.5,50)
-v_vals = np.linspace(-1,1.5,50)
-z = np.zeros((len(u_vals),len(v_vals)))
-
-for i in range(len(u_vals)):
-	for j in range(len(v_vals)):
-		z[i,j] = mapFeaturePlot(u_vals[i],v_vals[j],6) @ theta
+		result = 'Iterations = %d, lambda = %.1f, Accuracy = %.2f%%' % (iterations, Lambda, accuracy)
 		
-plt.contour(u_vals,v_vals,z.T,0)
-plt.ylabel('Microchip Test 2')
-plt.xlabel('Microchip Test 1')
-plt.legend()
-plt.show()
+		print(result)
 
+		#plot_classification(positive, negative, theta, result)
